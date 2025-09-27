a6c0f0f8b3b930597c68df30aecd4c13699156ff
Gabriel Lincoln <g97santos@gmail.com>
2025-09-28 00:20:42 +0200

    add fail tests for telemetry agent config

--- internal/agent/telemetry_test.go
+++ internal/agent/telemetry_test.go
@@ -7,109 +7,11 @@ import (
 
 	"github.com/LincolnG4/iot-hydra/internal/auth"
 	"github.com/LincolnG4/iot-hydra/internal/brokers"
-	"github.com/LincolnG4/iot-hydra/internal/brokers/nats"
 	"github.com/LincolnG4/iot-hydra/internal/message"
 	"github.com/rs/zerolog/log"
-	"github.com/stretchr/testify/assert"
-	"gopkg.in/yaml.v3"
 )
 
-func TestUnmarshalYAML_Success(t *testing.T) {
-	y := []byte(`
-telemetryAgent:
-  queueSize: 1000
-  maxWorkers: 2
-  brokers:
-    - name: ligmaMyNats
-      type: nats
-      address: "localhost:4222"
-      auth:
-        method: plain
-        user: test
-        password: testpass
-    - name: foo
-      type: nats
-      address: "localhost:9999"
-      auth:
-        method: token
-        token: my-secret-token
-`)
-	var wrapper struct {
-		TelemetryAgent `yaml:"telemetryAgent"`
-	}
-	err := yaml.Unmarshal(y, &wrapper)
-	assert.NoError(t, err)
-
-	// check global properties
-	assert.Equal(t, 1000, len(wrapper.TelemetryAgent.Queue), "Wrong queueSize")
-	assert.Len(t, wrapper.TelemetryAgent.Brokers, 2, "Expected to parse 2 brokers")
-
-	// Define test cases for each broker
-	testCases := []struct {
-		name             string
-		brokerName       string
-		expectedType     string
-		expectedAddress  string
-		expectedAuthType string
-		assertion        func(t *testing.T, b brokers.Broker)
-	}{
-		{
-			name:             "NATS broker with plain auth",
-			brokerName:       "ligmaMyNats",
-			expectedType:     "nats",
-			expectedAddress:  "localhost:4222",
-			expectedAuthType: "plain",
-			assertion: func(t *testing.T, b brokers.Broker) {
-				natsBroker, ok := b.(*nats.NATS)
-				assert.True(t, ok, "Broker should be a NATS broker")
-
-				basicAuth, ok := natsBroker.Config.Auth.(*auth.BasicAuth)
-				assert.True(t, ok, "Auth method should be BasicAuth")
-				assert.Equal(t, "test", basicAuth.Username, "Wrong username")
-				assert.Equal(t, "testpass", basicAuth.Password, "Wrong password")
-			},
-		},
-		{
-			name:             "NATS broker with token auth",
-			brokerName:       "foo",
-			expectedType:     "nats",
-			expectedAddress:  "localhost:9999",
-			expectedAuthType: "natsToken",
-			assertion: func(t *testing.T, b brokers.Broker) {
-				natsBroker, ok := b.(*nats.NATS)
-				assert.True(t, ok, "Broker should be a NATS broker")
-
-				tokenAuth, ok := natsBroker.Config.Auth.(*auth.TokenAuth)
-				assert.True(t, ok, "Auth method should be TokenAuth")
-				assert.Equal(t, "my-secret-token", tokenAuth.Token, "Wrong token")
-			},
-		},
-	}
-
-	// Iterate through all test cases
-	for _, tc := range testCases {
-		t.Run(tc.name, func(t *testing.T) {
-			// Check if the broker was created on the map
-			b, ok := wrapper.TelemetryAgent.Brokers[tc.brokerName]
-			assert.True(t, ok, "Broker '%s' not found in map", tc.brokerName)
-			b, _ = b.(*nats.NATS)
-			// Assert common properties
-			assert.Equal(t, tc.expectedType, b.Type(), "Wrong broker type")
-
-			// Assert specific broker address
-			if natsBroker, ok := b.(*nats.NATS); ok {
-				assert.Equal(t, tc.expectedAddress, natsBroker.Config.URL, "Wrong broker address")
-			}
-
-			// Run specific assertions for this broker type
-			tc.assertion(t, b)
-		})
-	}
-}
-
-// TODO: Create fail tests
-func TestUnmarshalYAML_Fail(t *testing.T) {}
-
+// TODO: Create tests
 func TestSendMessage_Success(t *testing.T) {
 	cfg := brokers.Config{
 		Name:    "nats",
--- internal/config/timeseriesAgent_test.go
+++ internal/config/timeseriesAgent_test.go
@@ -3,6 +3,7 @@ package config
 import (
 	"testing"
 
+	"github.com/LincolnG4/iot-hydra/internal/utils"
 	"github.com/alecthomas/assert"
 	"gopkg.in/yaml.v3"
 )
@@ -72,4 +73,147 @@ telemetryAgent:
 			assert.Equal(t, tc.expectedAuthType, b.Auth.Method, "Wrong broker address") // Assert Auth Method
 		})
 	}
+}
+
+func TestUnmarshalYAML_Fail_InvalidQueueSize(t *testing.T) {
+	y := []byte(`
+telemetryAgent:
+  queueSize: 0
+  maxWorkers: 2
+  brokers:
+    - name: foo
+      type: nats
+      address: "localhost:9999"
+      auth:
+        method: token
+        token: my-secret-token
+`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	err := yaml.Unmarshal(y, &wrapper)
+	assert.NoError(t, err) // YAML parsing should succeed
+
+	err = utils.Validate.Struct(wrapper.TelemetryAgent)
+	assert.Error(t, err)
+}
+
+func TestUnmarshalYAML_Fail_InvalidMaxWorkers(t *testing.T) {
+	y := []byte(`
+telemetryAgent:
+  queueSize: 100
+  maxWorkers: 0
+  brokers:
+    - name: foo
+      type: nats
+      address: "localhost:9999"
+      auth:
+        method: token
+        token: my-secret-token
+`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	_ = yaml.Unmarshal(y, &wrapper)
+
+	err := utils.Validate.Struct(wrapper.TelemetryAgent)
+	assert.Error(t, err)
+}
+
+func TestUnmarshalYAML_Fail_NoBrokers(t *testing.T) {
+	y := []byte(`
+telemetryAgent:
+  queueSize: 100
+  maxWorkers: 2
+  brokers: []
+`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	_ = yaml.Unmarshal(y, &wrapper)
+
+	err := utils.Validate.Struct(wrapper.TelemetryAgent)
+	assert.Error(t, err)
+}
+
+func TestUnmarshalYAML_Fail_MissingBrokerFields(t *testing.T) {
+	y := []byte(`
+telemetryAgent:
+  queueSize: 100
+  maxWorkers: 2
+  brokers:
+    - name: ""     # missing
+      type: ""     # missing
+      address: ""  # missing
+      auth:
+        method: "" # missing
+`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	_ = yaml.Unmarshal(y, &wrapper)
+
+	err := utils.Validate.Struct(wrapper.TelemetryAgent)
+	assert.Error(t, err)
+}
+
+func TestUnmarshalYAML_Fail_AuthMethodButMissingCredentials(t *testing.T) {
+	// "plain" auth requires user+password, but they're missing
+	y := []byte(`
+telemetryAgent:
+  queueSize: 100
+  maxWorkers: 2
+  brokers:
+    - name: foo
+      type: nats
+      address: "localhost:9999"
+      auth:
+        method: plain
+`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	_ = yaml.Unmarshal(y, &wrapper)
+
+	err := utils.Validate.Struct(wrapper.TelemetryAgent)
+	assert.NoError(t, err, "validation doesn't know about conditional fields")
+}
+
+func TestUnmarshalYAML_Fail_EmptyConfig(t *testing.T) {
+	y := []byte(`{}`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	_ = yaml.Unmarshal(y, &wrapper)
+
+	err := utils.Validate.Struct(wrapper.TelemetryAgent)
+	assert.Error(t, err)
+}
+
+func TestUnmarshalYAML_Fail_UnknownField(t *testing.T) {
+	y := []byte(`
+telemetryAgent:
+  queueSize: 100
+  maxWorkers: 2
+  brokers:
+    - name: foo
+      type: nats
+      address: "localhost:9999"
+      auth:
+        method: token
+        token: my-secret-token
+  unknownField: true
+`)
+
+	var wrapper struct {
+		TelemetryAgent TelemetryAgentYAML `yaml:"telemetryAgent"`
+	}
+	err := yaml.Unmarshal(y, &wrapper)
+	assert.NoError(t, err, "yaml.Unmarshal ignores unknown fields by default")
 }
